406ddb7d53681e12ad9dbaa854e637f320a183ab
Ori Elbaz <83533131+OriElbaz@users.noreply.github.com>
2025-06-20 11:45:02 +1000

    Feat: Clean up functions and .js file
    Create subfunctions for each step in game process.
    Add comments to easily represent where the
    different functions and variables are

--- javascript.js
+++ javascript.js
@@ -1,19 +1,37 @@
 
-
+// ** GLOBAL VARIABLES **
 let game = 1;
 let humanScore = 0;
 let computerScore = 0;
 let drawMatches = 0;
-// let letterS = '';
+const youScore = document.querySelector('.score-player');
+const pcScore = document.querySelector('.score-pc');
 
-// humans outcome
-let test = '';
-// pc outcome
-let pcOutcome = '';
-// outcome of round for player
-let outcome = '';
+// ** PLAY CHOICE FUNCTIONS **
+let humanChoiceRock = function rock(){
+    const test = 'ROCK';
+    const computersChoice = document.querySelector('.pc-choice-text');
+    computersChoice.textContent = randomRPS();
+    const pcOutcome = computersChoice.textContent;
+    evaluateRoundResult(test, pcOutcome)
+}
+let humanChoicePaper = function paper(){
+    const test = 'PAPER';
+    const computersChoice = document.querySelector('.pc-choice-text');
+    computersChoice.textContent = randomRPS();
+    const pcOutcome = computersChoice.textContent
+    evaluateRoundResult(test, pcOutcome)
+}
+let humanChoiceScissors = function scissors(){
+    const test = 'SCISSORS';
+    const computersChoice = document.querySelector('.pc-choice-text');
+    computersChoice.textContent = randomRPS();
+    const pcOutcome = computersChoice.textContent
+    evaluateRoundResult(test, pcOutcome)
+    
+}
 
-// PC's CHOICE
+// ** PC's CHOICE RANDOMISER **
 function randomRPS(){
     let randomNum = Math.random();
     let answer = '';
@@ -26,145 +44,17 @@ function randomRPS(){
     }
 }
 
-const youScore = document.querySelector('.score-player');
-const pcScore = document.querySelector('.score-pc');
-
-let humanChoiceRock = function rock(){
-    let test = 'ROCK';
-    const computersChoice = document.querySelector('.pc-choice-text');
-    computersChoice.textContent = randomRPS();
-    pcOutcome = computersChoice.textContent
-    if (playRound(test, pcOutcome) === 'Win'){
-        humanScore++
-        youScore.textContent = humanScore;
-        if (humanScore === 5){
-            alert("YOU WIN! GAMES OVER!")
-            humanScore = 0;
-            computerScore = 0;
-            pcScore.textContent = '0';
-            youScore.textContent = '0';
-        } else if (computerScore === 5){
-            alert("PC WINS! GAMES OVER!")
-            humanScore = 0;
-            computerScore = 0;
-            pcScore.textContent = '0';
-            youScore.textContent = '0';
-        }
-    } else if (playRound(test, pcOutcome) === 'Lose'){
-        computerScore++
-        pcScore.textContent = computerScore;
-        if (humanScore === 5){
-            alert("YOU WIN! GAMES OVER!")
-            humanScore = 0;
-            computerScore = 0;
-            pcScore.textContent = '0';
-            youScore.textContent = '0';
-        } else if (computerScore === 5){
-            alert("PC WINS! GAMES OVER!")
-            humanScore = 0;
-            computerScore = 0;
-            pcScore.textContent = '0';
-            youScore.textContent = '0';
-        }
+// ** SUB FUNCTIONS **
+let evaluateRoundResult = (test, pcOutcome)=>{
+    const result = playRound(test, pcOutcome)
+    if (result === 'Win'){
+        playRoundWin()
+    } else if (result === 'Lose'){
+        playRoundLose()
     } else {
         drawMatches++
     }
 }
-
-let humanChoicePaper = function paper(){
-    let test = 'PAPER';
-    const computersChoice = document.querySelector('.pc-choice-text');
-    computersChoice.textContent = randomRPS();
-    pcOutcome = computersChoice.textContent
-    if (playRound(test, pcOutcome) === 'Win'){
-        humanScore++
-        youScore.textContent = humanScore;
-        if (humanScore === 5){
-            alert("YOU WIN! GAMES OVER!")
-            humanScore = 0;
-            computerScore = 0;
-            pcScore.textContent = '0';
-            youScore.textContent = '0';
-        } else if (computerScore === 5){
-            alert("PC WINS! GAMES OVER!")
-            humanScore = 0;
-            computerScore = 0;
-            pcScore.textContent = '0';
-            youScore.textContent = '0';
-        }
-    } else if (playRound(test, pcOutcome) === 'Lose'){
-        computerScore++
-        pcScore.textContent = computerScore;
-        if (humanScore === 5){
-            alert("YOU WIN! GAMES OVER!")
-            humanScore = 0;
-            computerScore = 0;
-            pcScore.textContent = '0';
-            youScore.textContent = '0';
-        } else if (computerScore === 5){
-            alert("PC WINS! GAMES OVER!")
-            humanScore = 0;
-            computerScore = 0;
-            pcScore.textContent = '0';
-            youScore.textContent = '0';
-        }
-    } else {
-        drawMatches++
-    }
-}
-
-let humanChoiceScissors = function scissors(){
-    let test = 'SCISSORS';
-    const computersChoice = document.querySelector('.pc-choice-text');
-    computersChoice.textContent = randomRPS();
-    pcOutcome = computersChoice.textContent
-    if (playRound(test, pcOutcome) === 'Win'){
-        humanScore++
-        youScore.textContent = humanScore;
-        if (humanScore === 5){
-            alert("YOU WIN! GAMES OVER!")
-            humanScore = 0;
-            computerScore = 0;
-            pcScore.textContent = '0';
-            youScore.textContent = '0';
-        } else if (computerScore === 5){
-            alert("PC WINS! GAMES OVER!")
-            humanScore = 0;
-            computerScore = 0;
-            pcScore.textContent = '0';
-            youScore.textContent = '0';
-        }
-    } else if (playRound(test, pcOutcome) === 'Lose'){
-        computerScore++
-        pcScore.textContent = computerScore;
-        if (humanScore === 5){
-            alert("PC WINS! GAMES OVER!")
-            humanScore = 0;
-            computerScore = 0;
-            pcScore.textContent = '0';
-            youScore.textContent = '0';
-        } else if (computerScore === 5){
-            alert("PC WINS! GAMES OVER!")
-            humanScore = 0;
-            computerScore = 0;
-            pcScore.textContent = '0';
-            youScore.textContent = '0';
-        }
-    } else {
-        drawMatches++
-    }
-    
-}
-
-const buttonClickRock = document.querySelector('.rock-button');
-buttonClickRock.addEventListener('click', humanChoiceRock);
-
-const buttonClickPaper = document.querySelector('.paper-button');
-buttonClickPaper.addEventListener('click', humanChoicePaper);
-
-const buttonClickScissors = document.querySelector('.scissors-button');
-buttonClickScissors.addEventListener('click', humanChoiceScissors);
-
 function playRound(human, computer){  
     if (human === 'ROCK'){
         if (computer === 'Rock'){
@@ -193,39 +83,58 @@ function playRound(human, computer){
     }
     
 }
- 
+let playGame = ()=>{
+    const computersChoice = document.querySelector('.pc-choice-text');
+    computersChoice.textContent = randomRPS();
+    pcOutcome = computersChoice.textContent
+    if (playRound(test, pcOutcome) === 'Win'){
+        playRoundWin()
+    } else if (playRound(test, pcOutcome) === 'Lose'){
+        playRoundLose()
+    } else {
+        drawMatches++
+    }
+}
+let playRoundWin = ()=>{
+    humanScore++
+        youScore.textContent = humanScore;
+        // if scores reach 5
+        if (humanScore === 5){
+            gameEndPlayer()
+        } else if (computerScore === 5){
+            gameEndPC()
+        }
+}
+let playRoundLose = ()=>{
+    computerScore++
+    pcScore.textContent = computerScore;
+    if (humanScore === 5){
+        gameEndPlayer()
+    } else if (computerScore === 5){
+        gameEndPC()
+    }
+}
+let gameEndPC = ()=>{
+    alert("PC WINS! GAMES OVER!")
+    humanScore = 0;
+    computerScore = 0;
+    pcScore.textContent = '0';
+    youScore.textContent = '0';
+    const pcChoiceText = document.querySelector('.pc-choice-text')
+    pcChoiceText.textContent = '';
+}
+let gameEndPlayer = ()=>{
+    alert("YOU WIN! GAMES OVER!")
+    humanScore = 0;
+    computerScore = 0;
+    pcScore.textContent = '0';
+    youScore.textContent = '0';
+}
 
-
-// blank function
-// function nothing(){
-//     return 0;
-// }
-
-// function playGame(){
-    
-//     while (game <= 5){
-//         getHumanChoice()
-//         randomRPS()
-//         playRound()
-//         if (playRound(newUserAns, comAnswer) === 'Draw'){
-//             drawMatches++
-//         } else if (playRound(newUserAns, comAnswer) === 'Win'){
-//             humanScore++
-//         } else {
-//             computerScore++
-//         }
-//         game++
-//     }
-//     if (drawMatches > 1){
-//             letterS = 's';
-//         }
-
-//     console.log(`After all 5 games the scores are:`)
-//     console.log(`You: ${humanScore}`)
-//     console.log(`Computer: ${computerScore}`)
-//     console.log(`With ${drawMatches} draw${letterS}.`)
-// }
-
-
-
-// playGame()
+// ** BUTTON VARIABLES **
+const buttonClickRock = document.querySelector('.rock-button');
+    buttonClickRock.addEventListener('click', humanChoiceRock);
+const buttonClickPaper = document.querySelector('.paper-button');
+    buttonClickPaper.addEventListener('click', humanChoicePaper);
+const buttonClickScissors = document.querySelector('.scissors-button');
+    buttonClickScissors.addEventListener('click', humanChoiceScissors);
